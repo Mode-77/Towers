7c0d59af6a7079c22370a3d211c0e3359a1bb757
Jared Thomas <wjthomas@mail.roanoke.edu>
2023-01-23 22:48:30 -0500

    Added credits

--- help.cpp
+++ help.cpp
@@ -21,6 +21,10 @@
 // The passed in towers will be shown as a demonstration in the help text.
 void showHelpText(const std::vector<Tower>& towers, const TowerDrawer& towerDrawer)
 {
+    std::cout << "Towers, an adaptation of the game \"Tower of Hanoi\"\n";
+    std::cout << "Nexus Game Studios, 2023\n";
+    std::cout << "Programming, Jared Thomas\n";
+    std::cout << "\n";
     std::cout << "The goal of Towers is to move all the disks from the leftmost rod to the rightmost rod.\n";
     std::cout << "Sounds easy, right? But not so fast!\n";
     std::cout << "You can only move the topmost disk from any tower.\n";
